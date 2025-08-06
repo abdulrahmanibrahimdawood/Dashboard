d404c0f97b6572dd88b9638f96637115fa9d3926
Abdulrahman Ibrahim <abdoibrahim9111@gmail.com>
2025-08-06 23:17:29 +0300

    income section done

--- lib/views/widgets/dashboard_desktop_layout.dart
+++ lib/views/widgets/dashboard_desktop_layout.dart
@@ -1,6 +1,7 @@
 import 'package:dash_board/views/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:dash_board/views/widgets/custom_drawer.dart';
-import 'package:dash_board/views/widgets/income.dart';
+import 'package:dash_board/views/widgets/income_section.dart';
+import 'package:dash_board/views/widgets/my_card_and_transaction_history_section.dart';
 import 'package:flutter/material.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
@@ -14,7 +15,16 @@ class DashboardDesktopLayout extends StatelessWidget {
         SizedBox(width: 32),
         Expanded(flex: 2, child: AllExpensesAndQuickInvoiceSection()),
         SizedBox(width: 24),
-        Expanded(child: IncomeSection()),
+        Expanded(
+          child: Column(
+            children: [
+              SizedBox(height: 40),
+              MyCardAndTransactionHistorySection(),
+              SizedBox(height: 24),
+              Expanded(child: IncomeSection()),
+            ],
+          ),
+        ),
       ],
     );
   }
--- lib/views/widgets/income_section.dart
+++ lib/views/widgets/income_section.dart
@@ -1,5 +1,6 @@
 import 'package:dash_board/views/widgets/custom_back_ground_container.dart';
 import 'package:dash_board/views/widgets/income_chart.dart';
+import 'package:dash_board/views/widgets/income_details.dart';
 import 'package:dash_board/views/widgets/income_section_header.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +14,12 @@ class IncomeSection extends StatelessWidget {
         children: [
           IncomeSectionHeader(),
           Expanded(
-            child: Row(children: [Expanded(child: IncomeChart())]),
+            child: Row(
+              children: [
+                Expanded(child: IncomeChart()),
+                Expanded(child: IncomeDetails()),
+              ],
+            ),
           ),
         ],
       ),
